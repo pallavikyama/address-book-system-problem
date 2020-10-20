44b6e1523234d4b8d723d461c6b4df1d80cce65f
pallavikyama <kyamap98@gmail.com>
2020-10-20 10:52:29 +0530

    Modified:A new contact is added through console and printed.

--- Contact.java
+++ Contact.java
@@ -1,11 +1,36 @@
 package com.blz.addressbooksystem;
 
+import java.util.Scanner;
+
 public class Contact {
 	// VARIABLES
 	private String firstName, lastName, address, city, state, emailID;
 	private long zipCode, phoneNumber;
+	Scanner input = new Scanner(System.in);
 
 	public Contact() {
+		addContact();
+	}
+
+	// ADD NEW CONTACT INTO ADDRESS BOOK
+	public void addContact() {
+		System.out.println("\nCreating a new contact.\nEnter your contact details:");
+		System.out.println("First Name: ");
+		this.firstName = input.nextLine();
+		System.out.println("Last Name: ");
+		this.lastName = input.nextLine();
+		System.out.println("Address: ");
+		this.address = input.nextLine();
+		System.out.println("City: ");
+		this.city = input.nextLine();
+		System.out.println("State: ");
+		this.state = input.nextLine();
+		System.out.println("Zip Code: ");
+		this.zipCode = input.nextLong();
+		System.out.println("Phone Number: ");
+		this.phoneNumber = input.nextLong();
+		System.out.println("E-mail ID: ");
+		this.emailID = input.next();
 		printContacts();
 	}
 
