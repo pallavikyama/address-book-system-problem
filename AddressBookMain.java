d23420d3013e969793f44a69f9b5ce3f3e3c1271
pallavikyama <kyamap98@gmail.com>
2020-10-20 13:08:13 +0530

    Modified:Multiple Contacts are added to the address book and maintained using collection class.

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,6 +3,6 @@ package com.blz.addressbooksystem;
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to ADDRESS BOOK SYSTEM Program.");
-		new Contact();
+		new Contact().contactMenu();
 	}
 }
--- Contact.java
+++ Contact.java
@@ -1,5 +1,6 @@
 package com.blz.addressbooksystem;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Contact {
@@ -7,10 +8,7 @@ public class Contact {
 	private String firstName, lastName, address, city, state, emailID;
 	private long zipCode, phoneNumber;
 	Scanner input = new Scanner(System.in);
-
-	public Contact() {
-		addContact();
-	}
+	ArrayList<Contact> book = new ArrayList<Contact>();
 
 	// ADDRESS BOOK MENU
 	public void contactMenu() {
@@ -20,12 +18,14 @@ public class Contact {
 			while (!valid) {
 				try {
 					System.out.println("\nAddress-Book Menu:\n(Select any one operation from the menu below)"
-							+ "\n1.Edit first name of existing contact" + "\n2.Edit last name of existing contact"
-							+ "\n3.Edit residence address of existing contact"
-							+ "\n4.Edit residing city of existing contact"
-							+ "\n5.Edit residing state of existing contact"
-							+ "\n6.Edit zip/pin code of existing contact" + "\n7.Edit phone number of existing contact"
-							+ "\n8.Edit email-ID of existing contact" + "\n9.Delete existing contact"
+							+ "\n1.Add a new contact" + "\n2.Edit first name of existing contact"
+							+ "\n3.Edit last name of existing contact"
+							+ "\n4.Edit residence address of existing contact"
+							+ "\n5.Edit residing city of existing contact"
+							+ "\n6.Edit residing state of existing contact"
+							+ "\n7.Edit zip/pin code of existing contact" + "\n8.Edit phone number of existing contact"
+							+ "\n9.Edit email-ID of existing contact" + "\n10.Delete existing contact"
+							+ "\n11.Print all contacts in this address book"
 							+ "\nPress any other integer to exit address book");
 					choice = input.nextInt();
 					if (choice >= 0)
@@ -38,163 +38,224 @@ public class Contact {
 			input.nextLine(); // CLEAR THE INPUT
 			switch (choice) {
 			case 1: {
-				editFirstName();
+				addContact();
 				break;
 			}
 			case 2: {
-				editLastName();
+				editFirstName();
+				emptyBookCheck();
+				if (!book.isEmpty())
+					editFirstName();
 				break;
 			}
 			case 3: {
-				editAddress();
+				editLastName();
+				emptyBookCheck();
+				if (!book.isEmpty())
+					editLastName();
 				break;
 			}
 			case 4: {
-				editCity();
+				editAddress();
+				emptyBookCheck();
+				if (!book.isEmpty())
+					editAddress();
 				break;
 			}
 			case 5: {
-				editState();
+				editCity();
+				emptyBookCheck();
+				if (!book.isEmpty())
+					editCity();
 				break;
 			}
 			case 6: {
-				editZipCode();
+				editState();
+				emptyBookCheck();
+				if (!book.isEmpty())
+					editState();
 				break;
 			}
 			case 7: {
-				editPhoneNo();
+				editZipCode();
+				emptyBookCheck();
+				if (!book.isEmpty())
+					editZipCode();
 				break;
 			}
 			case 8: {
-				editEmailID();
+				editPhoneNo();
+				emptyBookCheck();
+				if (!book.isEmpty())
+					editPhoneNo();
 				break;
 			}
-			case 9:
-				deleteContact();
+			case 9: {
+				editEmailID();
+				emptyBookCheck();
+				if (!book.isEmpty())
+					editEmailID();
+				break;
+			}
+			case 10: {
+				emptyBookCheck();
+				if (!book.isEmpty())
+					deleteContact();
+				break;
+			}
+			case 11: {
+				emptyBookCheck();
+				if (!book.isEmpty())
+					printContacts();
+				break;
+			}
 			default: {
-				System.out.println("Closing Address Book. ");
+				input.close();
+				System.out.println("Closing Address Book.");
 				return;
 			}
 			}
-		} while (choice > 0 && choice < 9);
+		} while (choice > 0 && choice <= 11);
 	}
 
-	// ADD NEW CONTACT INTO ADDRESS BOOK
+	// 1. ADD NEW CONTACT INTO ADDRESS BOOK
 	public void addContact() {
+		Contact person = new Contact();
 		System.out.println("\nCreating a new contact.\nEnter your contact details:");
-		System.out.println("First Name: ");
-		this.firstName = input.nextLine();
-		System.out.println("Last Name: ");
-		this.lastName = input.nextLine();
-		System.out.println("Address: ");
-		this.address = input.nextLine();
-		System.out.println("City: ");
-		this.city = input.nextLine();
-		System.out.println("State: ");
-		this.state = input.nextLine();
-		System.out.println("Zip Code: ");
-		this.zipCode = input.nextLong();
-		System.out.println("Phone Number: ");
-		this.phoneNumber = input.nextLong();
-		System.out.println("E-mail ID: ");
-		this.emailID = input.next();
-		printContacts();
-		contactMenu();
+		System.out.print("First Name: ");
+		person.firstName = input.nextLine();
+		System.out.print("Last Name: ");
+		person.lastName = input.nextLine();
+		System.out.print("Address: ");
+		person.address = input.nextLine();
+		System.out.print("City: ");
+		person.city = input.nextLine();
+		System.out.print("State: ");
+		person.state = input.nextLine();
+		System.out.print("Zip Code: ");
+		person.zipCode = input.nextLong();
+		System.out.print("Phone Number: ");
+		person.phoneNumber = input.nextLong();
+		System.out.print("E-mail ID: ");
+		person.emailID = input.next();
+		book.add(person);
+		System.out.println("\nContact details: " + person.toString());
 	}
 
-	public boolean getContactName() {
+	private Contact getContact() {
 		System.out.println("\nPlease enter your first name(as previously stored in the address book): ");
 		String name = input.nextLine();
-		if (this.firstName.equals(name))
-			return true;
-		else
-			System.out.println("No such first name exists in the address book.");
-		return false;
+		boolean validName = false;
+		for (Contact item : book) {
+			if (item.firstName.equals(name)) {
+				validName = true;
+				return item;
+			}
+		}
+		if (!validName)
+			System.out.println("\nNo such first name exists in the address book.");
+		return null;
 	}
 
-	// 1. UPDATE FIRST NAME IN ADDRESS BOOK
-	public void editFirstName() {
-		if (getContactName()) {
+	// 2. UPDATE FIRST NAME IN ADDRESS BOOK
+	private void editFirstName() {
+		Contact person = getContact();
+		if (person != null) {
 			System.out.println("Enter new first name:");
-			this.firstName = input.nextLine();
-			printContacts();
+			person.firstName = input.nextLine();
+			System.out.println("\nContact details: " + person.toString());
 		}
 	}
 
-	// 2. UPDATE LAST NAME IN ADDRESS BOOK
-	public void editLastName() {
-		if (getContactName()) {
+	// 3. UPDATE LAST NAME IN ADDRESS BOOK
+	private void editLastName() {
+		Contact person = getContact();
+		if (person != null) {
 			System.out.println("Enter new last name:");
-			this.lastName = input.nextLine();
-			printContacts();
+			person.lastName = input.nextLine();
+			System.out.println("\nContact details: " + person.toString());
 		}
 	}
 
-	// 3. UPDATE RESIDENCE ADDRESS IN ADDRESS BOOK
-	public void editAddress() {
-		if (getContactName()) {
+	// 4. UPDATE RESIDENCE ADDRESS IN ADDRESS BOOK
+	private void editAddress() {
+		Contact person = getContact();
+		if (person != null) {
 			System.out.println("Enter new address:");
-			this.address = input.nextLine();
-			printContacts();
+			person.address = input.nextLine();
+			System.out.println("\nContact details: " + person.toString());
 		}
 	}
 
-	// 4. UPDATE RESIDING CITY IN ADDRESS BOOK
+	// 5. UPDATE RESIDING CITY IN ADDRESS BOOK
 	private void editCity() {
-		if (getContactName()) {
+		Contact person = getContact();
+		if (person != null) {
 			System.out.println("Enter new city:");
-			this.city = input.nextLine();
-			printContacts();
+			person.city = input.nextLine();
+			System.out.println("\nContact details: " + person.toString());
 		}
 	}
 
-	// 5. UPDATE RESIDING STATE IN ADDRESS BOOK
-	public void editState() {
-		if (getContactName()) {
+	// 6. UPDATE RESIDING STATE IN ADDRESS BOOK
+	private void editState() {
+		Contact person = getContact();
+		if (person != null) {
 			System.out.println("Enter new state:");
-			this.state = input.nextLine();
-			printContacts();
+			person.state = input.nextLine();
+			System.out.println("\nContact details: " + person.toString());
 		}
 	}
 
-	// 6. UPDATE ZIP CODE IN ADDRESS BOOK
-	public void editZipCode() {
-		if (getContactName()) {
+	// 7. UPDATE ZIP CODE IN ADDRESS BOOK
+	private void editZipCode() {
+		Contact person = getContact();
+		if (person != null) {
 			System.out.println("Enter new zip-code:");
-			this.zipCode = input.nextLong();
-			printContacts();
+			person.zipCode = input.nextLong();
+			System.out.println("\nContact details: " + person.toString());
 		}
 	}
 
-	// 7. UPDATE PHONE NUMBER IN ADDRESS BOOK
-	public void editPhoneNo() {
-		if (getContactName()) {
+	// 8. UPDATE PHONE NUMBER IN ADDRESS BOOK
+	private void editPhoneNo() {
+		Contact person = getContact();
+		if (person != null) {
 			System.out.println("Enter new phone number:");
-			this.phoneNumber = input.nextLong();
-			printContacts();
+			person.phoneNumber = input.nextLong();
+			System.out.println("\nContact details: " + person.toString());
 		}
 	}
 
-	// 8. UPDATE RESIDING STATE IN ADDRESS BOOK
-	public void editEmailID() {
-		if (getContactName()) {
+	// 9. UPDATE RESIDING STATE IN ADDRESS BOOK
+	private void editEmailID() {
+		Contact person = getContact();
+		if (person != null) {
 			System.out.println("Enter new email-ID:");
-			this.emailID = input.next();
-			printContacts();
+			person.emailID = input.next();
+			System.out.println("\nContact details: " + person.toString());
 		}
 	}
 
-	// 9. DELETE EXISTING CONTACT DETAILS
-	public void deleteContact() {
-		if (getContactName()) {
+	// 10. DELETE EXISTING CONTACT DETAILS
+	private void deleteContact() {
+		Contact person = getContact();
+		if (person != null) {
 			System.out.println("Deleting contact.");
-			return;
+			book.remove(person);
 		}
+		printContacts();
 	}
 
-	// DISPLAY THIS ADDRESS BOOK
-	public void printContacts() {
-		System.out.println("\nThis address book has stored the following:" + toString());
+	// 11. DISPLAY THIS ADDRESS BOOK
+	private void printContacts() {
+		emptyBookCheck();
+		if (!book.isEmpty()) {
+			System.out.println("\nThis address book has stored the following:");
+			for (Contact contact : book) {
+				System.out.println(contact.toString());
+			}
+		}
 	}
 
 	// TO PRINT CONTACT DETAILS
@@ -203,5 +264,11 @@ public class Contact {
 		return "\nFirst Name: " + this.firstName + "\nLast Name: " + this.lastName + "\nAddress: " + this.address
 				+ "\nCity: " + this.city + "\nState: " + this.state + "\nZip-code: " + this.zipCode + "\nPhone number: "
 				+ this.phoneNumber + "\nEmail-ID: " + this.emailID;
+	}
+
+	// CHECK IF ADDRESS BOOK IS EMPTY
+	public void emptyBookCheck() {
+		if (book.isEmpty())
+			System.out.println("\n========== EMPTY book! ==========");
 	}
 }
