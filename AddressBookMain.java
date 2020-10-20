bb6de0c0120752d232a363870b9e12a0f955b591
pallavikyama <kyamap98@gmail.com>
2020-10-20 10:06:38 +0530

    Added:Welcome message is added.

---
+++ AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.blz.addressbooksystem;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to ADDRESS BOOK SYSTEM Program.");
+	}
+}
